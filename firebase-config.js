2e59bf88adb65cb8064b972375275cf5ebc3333f
CHRIS KUO <chriskuo@CHRISs-Mac-mini.local>
2026-09-13 19:38:28 -0400

    Fix firebase-config.js format

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,15 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyAFwnIMs8JDNX6JnoH25CIB_uzKxwRNN_w",
+  authDomain: "eagle-en-game.firebaseapp.com",
+  projectId: "eagle-en-game",
+  storageBucket: "eagle-en-game.firebasestorage.app",
+  messagingSenderId: "964609515170",
+  appId: "1:964609515170:web:83135424315ff9bd7141fb"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAFwnIMs8JDNX6JnoH25CIB_uzKxwRNN_w",
-    authDomain: "eagle-en-game.firebaseapp.com",
-    projectId: "eagle-en-game",
-    storageBucket: "eagle-en-game.firebasestorage.app",
-    messagingSenderId: "964609515170",
-    appId: "1:964609515170:web:83135424315ff9bd7141fb",
-    measurementId: "G-3BYNZNHFT9"
-  };
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const AUTH_EMAIL_DOMAIN = "eagle-quest.local";
+const TEACHER_EMAIL = "teacher@eagle-quest-admin.local";
